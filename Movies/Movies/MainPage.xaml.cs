d5642e32080a35186ca9cd1f9e4181fe23e3f6d2
Roger Leblanc <roger@rogerleblanc.net>
2018-09-27 23:21:10 -0400

    Initial commit

---
+++ Movies/Movies/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Movies
+{
+    public partial class MainPage : ContentPage
+    {
+        private HttpClient _client;
+        private string _baseUrl = "https://www.omdbapi.com/?apikey=[YOUR_API_KEY]&";
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //No navigation bar on first screen
+            NavigationPage.SetHasNavigationBar(this, false);
+
+            SetNoResultsLabelIsVisible();
+        }
+
+        private void SetNoResultsLabelIsVisible()
+        {
+            noResultsLabel.IsVisible = listView.ItemsSource == null;
+        }
+
+        private async void searchBar_SearchButtonPressed(object sender, EventArgs e)
+        {
+            listView.ItemsSource = String.IsNullOrWhiteSpace(searchBar.Text) ?
+                null :
+                await GetMovies(searchBar.Text);
+
+            SetNoResultsLabelIsVisible();
+        }
+
+        private async Task<IList<Movie>> GetMovies(string searchText)
+        {
+            if (_client == null)
+                _client = new HttpClient();
+
+            try
+            {
+                HttpResponseMessage responseMessage = await _client.GetAsync(_baseUrl + "s=" + searchText);
+                string json = await responseMessage.Content.ReadAsStringAsync();
+                OmdbResult result = JsonConvert.DeserializeObject<OmdbResult>(json);
+                return result.Search;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        private void SearchBar_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(e.NewTextValue))
+            {
+                listView.ItemsSource = null;
+                SetNoResultsLabelIsVisible();
+            }
+        }
+
+        private async void listView_Refreshing(object sender, EventArgs e)
+        {
+            listView.ItemsSource = String.IsNullOrWhiteSpace(searchBar.Text) ?
+                null :
+                await GetMovies(searchBar.Text);
+
+            SetNoResultsLabelIsVisible();
+
+            listView.IsRefreshing = false;
+        }
+
+        private async void listView_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if (!(listView.SelectedItem is Movie movie))
+                return;
+
+            await Navigation.PushAsync(new MovieDetailPage(movie.imdbID));
+
+            listView.SelectedItem = null;
+        }
+    }
+
+    public class OmdbResult
+    {
+        public List<Movie> Search { get; set; }
+        public string totalResults { get; set; }
+        public string Response { get; set; }
+    }
+
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string imdbID { get; set; }
+        public string Type { get; set; }
+        public string Poster { get; set; }
+    }
+}
---
+++ Movies/Movies/MovieDetailPage.xaml.cs
@@ -0,0 +1,84 @@
+﻿
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Movies
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MovieDetailPage : ContentPage
+    {
+        private string _movieId;
+        private HttpClient _client;
+        private string _baseUrl = "https://www.omdbapi.com/?apikey=[YOUR_API_KEY]&plot=full&";
+
+        public MovieDetailPage(string movieId)
+        {
+            InitializeComponent();
+            _movieId = movieId;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            IsBusy = true;
+
+            if (_client == null)
+                _client = new HttpClient();
+
+            try
+            {
+                HttpResponseMessage responseMessage = await _client.GetAsync(_baseUrl + "i=" + _movieId);
+                string json = await responseMessage.Content.ReadAsStringAsync();
+                MovieDetail movieDetail = JsonConvert.DeserializeObject<MovieDetail>(json);
+                BindingContext = movieDetail;
+            }
+            catch (Exception ex)
+            {
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+    }
+
+    public class MovieDetail
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Rated { get; set; }
+        public string Released { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Writer { get; set; }
+        public string Actors { get; set; }
+        public string Plot { get; set; }
+        public string Language { get; set; }
+        public string Country { get; set; }
+        public string Awards { get; set; }
+        public string Poster { get; set; }
+        public List<Rating> Ratings { get; set; }
+        public string Metascore { get; set; }
+        public string imdbRating { get; set; }
+        public string imdbVotes { get; set; }
+        public string imdbID { get; set; }
+        public string Type { get; set; }
+        public string DVD { get; set; }
+        public string BoxOffice { get; set; }
+        public string Production { get; set; }
+        public string Website { get; set; }
+        public string Response { get; set; }
+    }
+
+    public class Rating
+    {
+        public string Source { get; set; }
+        public string Value { get; set; }
+    }
+}
